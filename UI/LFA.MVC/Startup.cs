afc7288a3daed797ebf7fc510e6c327ae6a841e2
shrestharabin <rabin.shrestha@outlook.com>
2016-04-28 08:23:54 +0545

    First MVC Project

---
+++ UI/LFA.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LFA.MVC.Startup))]
+namespace LFA.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
