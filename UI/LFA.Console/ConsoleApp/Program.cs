a571a9782df59bf7f6798f955987126ef5ff0af7
shrestharabin <rabin.shrestha@outlook.com>
2016-05-03 21:39:43 +0545

    Authenticate function added..

--- UI/LFA.Console/ConsoleApp/Program.cs
+++ UI/LFA.Console/ConsoleApp/Program.cs
@@ -22,28 +22,57 @@ namespace ConsoleApp
 
             //}
 
-            objUser.UserID = 1;
-            objUser.UserName = "rabin";
-            objUser.Password = "password";
+            //objUser.UserID = 1;
+            //objUser.UserName = "rabin";
+            //objUser.Password = "password";
 
-            int count = 1;
-            var count2 = count;
-            count2 = 2;
+            //int count = 1;
+            //var count2 = count;
+            //count2 = 2;
 
-            var objNewUser = objUser;
-            objNewUser.UserName = "admin";
+            //var objNewUser = objUser;
+            //objNewUser.UserName = "admin";
 
-            Console.WriteLine(objUser.UserID);
-            Console.WriteLine(objUser.UserName);
-            Console.WriteLine(objUser.Password);
+            //Console.WriteLine(objUser.UserID);
+            //Console.WriteLine(objUser.UserName);
+            //Console.WriteLine(objUser.Password);
 
-            Console.WriteLine("The value of count is {0}", count);
-            Console.WriteLine("The value of count2 is {0}", count2);
+            //Console.WriteLine("The value of count is {0}", count);
+            //Console.WriteLine("The value of count2 is {0}", count2);
 
-            
+            //=>>>>>using void Authenticate
+            //objUser.Authenticate();
+           // objUser.Authenticate(password:"test");
 
-            Console.WriteLine();
+           
+            //=>>>>>using bool Authenticate
+
+            //if (objUser.Authenticate(userName: "test"))
+            //{
+            //    Console.WriteLine("True");
+                
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //=>>>>>using string Authenticate
+            string userName=objUser.Authenticate(userName: "admin");
+
+            if (userName=="admin")
+            {
+                Console.WriteLine("Welcome {0} ", userName);
+
+            }
+            else
+            {
+                Console.WriteLine("Non admin user found!!");
+            }
+
             Console.ReadLine();
+
+
 
         }
     }
---
+++ UI/LFA.Console/ConsoleApp/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class User
+    {
+        #region public Members
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        #endregion
+
+        
+        //public void Authenticate(string userName = "admin", string password = "password")
+        //{
+        //    if ((userName == "admin") && (password == "password"))
+        //    {
+        //        Console.WriteLine("welcome {0} you are logged in as Administrator", userName);
+        //    }
+        //    else if ((userName == "user") && (Password == "password"))
+        //    {
+        //        Console.WriteLine("welcome {0} you are logged in as Normal User", userName);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Invalid login");
+        //    }
+        //}
+
+
+        //public bool Authenticate(string userName = "admin", string password = "password")
+        //{
+        //    return
+        //    ((userName == "admin") && (password == "password")) || ((userName == "user") && (Password == "password"));
+        //}
+
+        public string Authenticate(string userName = "admin", string password = "password")
+        {
+            return userName; 
+            
+        }
+
+    }
+}
