a3ff05be0e1149ae4d4f8a7312410daa38a40ee2
shrestharabin <rabin.shrestha@outlook.com>
2016-06-05 08:55:11 +0545

    MVC_Users Updated

---
+++ SharedProjects/LFA.Data/LFA.Data/LFAComments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA.Data
+{
+    public class LFAComments
+    {
+        public int CommentId { get; set; }
+        public string Comment { get; set; }
+        public int CommenterId { get; set; }
+        public DateTime CommentDate { get; set; }
+        public sbyte IsDeleted { get; set; }
+    }
+}
---
+++ SharedProjects/LFA.Data/LFA.Data/LFAPosts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFA.Data
+{
+    public class LFAPosts
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostDetail { get; set; }
+        public DateTime PostDate { get; set; }
+        public sbyte IsDeleted { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
